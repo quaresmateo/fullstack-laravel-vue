adaa7619952ab4b6a7889cb22d8bf1de761f929e
quaresmateo <teoian.giovane@gmail.com>
2021-03-20 11:10:52 -0300

    Upadate filesystems files

--- src/config/filesystems.php
+++ src/config/filesystems.php
@@ -48,10 +48,15 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'local_public' => [
+            'driver' => 'local',
+            'root'   => public_path(),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
