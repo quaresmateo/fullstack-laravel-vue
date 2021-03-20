f3a9bba13b8d57b73b84335ea1e5134e2696d44a
quaresmateo <teoian.giovane@gmail.com>
2021-03-19 23:35:08 -0300

    Update TaskController.php

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -10,6 +10,53 @@ class TaskController extends Controller
 {
     public function index(Request $request, Response $response)
     {
+        $tasks = Task::all();
+
+        $response()->json([
+            'data' => $tasks,
+            'message' => 'all tasks'
+        ]);
+    }
+
+    public function show(Request $request, Response $response)
+    {
+        $task = Task::find($request->id);
+
+        return $response()->json([
+            'data' => $task,
+            'message' => 'created!'
+        ]);
+    }
+
+    public function update(Request $request, Response $response)
+    {
+        $task = Task::find($request->id);
+
+        $task->date = $request->date;
+        $task->type = $request->type;
+        $task->description = $request->description;
+        $task->check = $request->check;
+
+        $task->save();
+
+        $response()->json([
+            'data' => $task,
+            'message' => 'updated'
+        ]);
+    }
+
+    public function delete(Request $request, Response $response)
+    {
+        $task = Task::find($request->id);
+        $task->delete();
+
+        $response()->json([
+            'message' => 'deleted'
+        ]);
+    }
+
+    public function store(Request $request, Response $response)
+    {
         $task = new Task;
 
         $task->date = $request->date;
