aa068a815c50ed5d108e2a47c4376d01a4b7aefa
quaresmateo <teoian.giovane@gmail.com>
2021-03-19 21:58:02 -0300

    Creates routes

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
+use Facade\FlareClient\Http\Response;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function index(Request $request, Response $response)
+    {
+        $task = new Task;
+
+        $task->date = $request->date;
+        $task->type = $request->type;
+        $task->description = $request->description;
+        $task->check = $request->check;
+
+        $task->save();
+
+        return $response()->json([
+            'message' => 'created!',
+            'status' => '201'
+        ]);
+    }
 }
