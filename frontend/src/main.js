fb51c895d33b80f7ea1c81a6c7fd9fd59ce7503a
quaresmateo <teoian.giovane@gmail.com>
2021-03-22 09:34:16 -0300

    Remove vue-axios module

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,13 +5,10 @@ import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-Vue.use(VueAxios, axios)
 
 new Vue({
   router,
